cc7569cdabee0e539b61cdd3930ba15101a5e506
RandyBorn <randyborn677@yahoo.com>
2025-06-18 11:02:44 +0200

    portfolio bearbeitet

--- app/skills/page.js
+++ app/skills/page.js
@@ -20,17 +20,17 @@ export default function SkillsSection() {
   const [showSkills, setShowSkills] = useState(false);
 
   useEffect(() => {
-    const timeout = setTimeout(() => setShowSkills(true), 200);
+    const timeout = setTimeout(() => setShowSkills(true), 5);
     return () => clearTimeout(timeout);
   }, []);
 
   return (
     <section className="my-16">
-      <h2 className="text-3xl font-bold text-amber-50 text-center mb-10">
+      <h2 className="text-3xl font-bold wrap-normal text-amber-50 text-center mb-50">
         ⚙️ Tech Stack/Skills
       </h2>
 
-      <div className="grid grid-cols-3 sm:grid-cols-3 gap-10 max-w-7xl mx-auto">
+      <div className="grid grid-cols-3 sm:grid-cols-3 gap-15 max-w-7xl mx-auto kenburns-bottom">
         {skills.map((skill, index) => (
           <div
             key={index}
@@ -39,9 +39,9 @@ export default function SkillsSection() {
               opacity-0 translate-y-6
               ${showSkills ? "opacity-100 translate-y-0" : ""}
               delay-${index * 100}
-              bg-gray-800 text-gray-200 px-4 py-3 rounded-xl text-center shadow-md hover:scale-105 hover:bg-purple-600 hover:text-white cursor-default
+              bg-purple-600 text-amber-50 px-4 py-3 rounded-xl text-center shadow-md hover:scale-105 hover:bg-purple-600 hover:text-white cursor-default
             `}
-            style={{ transitionDelay: `${index * 300}ms` }}
+            style={{ transitionDelay: `${index * 700}ms` }}
           >
             {skill}
           </div>
